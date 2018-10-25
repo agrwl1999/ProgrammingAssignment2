536c3dcc8b42ad16ac1fdbe47c700df45e265acc
agrwl1999 <agrawalrahuludr@gmail.com>
2018-10-26 01:11:14 +0530

    Assignment
    Here is the small piece of code for the given assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Write a short comment describing this function
+
+## ------------------------------------------------------------------------
+
+## Retrieving the matrix inverse by the following R code
+## A set of two functions- "makeCacheMatrix" & "cacheSolve" are defined
+## First function "makeCacheMatrix" creates a special matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv<-NULL
+	set<-function(y){
+		x<<-y	
+		inv<<-NULL
+	}
+	get<-function()x
+	setInverse<-function(inverse) inv<<-inverse
+	getInverse<-function()inv
+	list(set=set,
+	     get=get,
+           setInverse=setInverse,
+           getInverse=getInverse)
 }
-
-
-## Write a short comment describing this function
+ 	
+## Second function "cacheSolve" calculates the inverse of that special matrix
+## created by first function defined above and then checking the condition 
+## that if inverse has already taken than in that case matrix inverse will be
+## retireved from the cache 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+	inv<- x$getInverse()
+      if(!is.null(inv)){
+		message("matrix retrieved")
+		return(inv)
+	}
+	mat<-x$get()
+	inv<-solve(mat,...)
+	x$setInverse(inv)
+	inv
 }
